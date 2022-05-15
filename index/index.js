8d87b467a049c1a5ebfbad9792e4a7eaef0ad69f
Erkoyan <erkoyanarman@gmail.com>
2022-05-16 00:13:27 +0400

    add index

--- index/index.js
+++ index/index.js
@@ -1,6 +1,11 @@
+var caunt = 2
 function myFunction(){
-    var img = document.createElement('img');
+    var img = document.getElementById('img');
 
-    img.src = "imgs/Screenshot_1.png"
-    console.log(toString.img.src)
+img.src = "imgs/Screenshot_"+caunt+".png";
+    caunt++;
+    if(caunt == 4){
+        caunt = 1;
+    }
+    // img.src = "imgs/Screenshot_"+caunt+".png";
 }
